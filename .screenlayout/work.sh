d4a414a02e16b027dc5b4e7df4b0c4605abe9372
Jared Allard <jaredallard@outlook.com>
2018-07-02 23:46:06 -0700

    feat: add screenlayout configs

---
+++ .screenlayout/work.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 6000x2360 --rotate normal --output DP1 --primary --mode 3840x2160 --pos 0x240 --rotate left --output HDMI2 --off --output HDMI1 --off --output DP2 --mode 3840x2160 --pos 2160x1280 --rotate normal

a2ff89f1737f518de132bf7eb3facdecf897c9a7
Jared Allard <jaredallard@outlook.com>
2018-08-26 15:38:51 -0700

    feat(polybar): use Fira Code bold everywhere now, now_playing fixes

--- .config/polybar/now_playing.sh
+++ .config/polybar/now_playing.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-song=$(playerctl metadata xesam:title)
-artist=$(playerctl metadata xesam:artist)
+song=$(playerctl metadata xesam:title 2>&1)
+artist=$(playerctl metadata xesam:artist 2>&1)
 
-if [[ $? -ne 0 ]]; then
+if grep "No players found" <<<"${song}" >/dev/null; then
+  echo ""
   exit 0
 fi
 

8e4c3310afec6979a8b37f0418cefa14ab751fd7
Jared Allard <jaredallard@outlook.com>
2019-03-23 11:32:34 -0700

    feat: update dotfiles

--- .config/i3/cycle-workspaces.py
+++ .config/i3/cycle-workspaces.py
@@ -1,7 +1,7 @@
-
-#!/usr/bin/env python
+#!/usr/bin/env python3
  
 import i3
+import random
 
 outputs = i3.get_outputs()
 workspaces = i3.get_workspaces()
@@ -10,8 +10,12 @@ workspaces = i3.get_workspaces()
 workspace = list(filter(lambda s: s['focused']==True, workspaces))
 output = list(filter(lambda s: s['active']==True, outputs))
 
-# figure out the other workspace name
-other_workspace = list(filter(lambda s: s['name']!=workspace[0]['output'], output))
+# figure out the other workspaces name
+other_workspaces = list(filter(lambda s: s['name']!=workspace[0]['output'], output))
+
+# pick a random workspace to put it onto, that's not the current one. (TODO: store / track the last ones we moved it onto)
+num_of_workspaces = len(other_workspaces)
+random_workspace = random.randint(0, num_of_workspaces-1)
 
 # send current to the no-active one
-i3.command('move', 'workspace to output '+other_workspace[0]['name'])
+i3.command('move', 'workspace to output '+other_workspaces[random_workspace]['name'])
--- .screenlayout/work.sh
+++ .screenlayout/work.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-1 --off --output eDP-1 --primary --mode 1920x1080 --pos 2520x1440 --rotate normal --output DP-2 --mode 2560x1440 --pos 0x0 --rotate normal
+xrandr --output DVI-I-1-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DVI-I-2-2 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --mode 1920x1080 --pos 3840x0 --rotate normal --output eDP-1 --off --output DP-2 --off

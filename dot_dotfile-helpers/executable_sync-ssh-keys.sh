338e2951f32ff3a800ba2f7bfa541bf17e1bc038
Jared Allard <jaredallard@outlook.com>
2020-04-04 14:06:51 -0700

    feat: initial commit using chezmoi

---
+++ dot_dotfile-helpers/executable_sync-ssh-keys.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+# Generates stub ssh keys to be imported via chezmoi
+set -e -o pipefail
+
+echo "fetching ssh keys ..."
+sshKeys=$(op list documents | jq -r '.[] | select(.overview.tags[0] != null) | .uuid+"|"+.overview.title')
+
+echo "creating ssh keys ..."
+while read key; do
+  uuid=$(awk -F '|' '{ print $1 }' <<< "$key")
+  fileName=$(awk -F '|' '{ print $2 }' <<< "$key")
+
+  keyPath="$HOME/.ssh/$fileName"
+  if [[ -e "$keyPath" ]]; then
+    continue
+  fi
+  
+  echo " --> importing '$fileName' (uuid: $uuid)"
+
+  cat > "$keyPath" << EOF
+{{- onepasswordDocument "$uuid" -}}
+EOF
+  
+  echo " --> adding '$fileName' to chemozi"
+  chezmoi add --template "$keyPath"
+done <<< "$sshKeys"
+echo "done"
---
+++ dot_screenlayout/executable_work.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --off --output DP1 --off --output DP2 --mode 1920x1080 --pos 3840x0 --rotate normal --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off --output DVI-I-2-2 --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-I-1-1 --mode 1920x1080 --pos 1920x0 --rotate normal

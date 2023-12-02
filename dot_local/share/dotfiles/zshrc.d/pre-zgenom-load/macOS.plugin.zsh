d24256ca8abe6a41f99a2054635903327e99ede9
Jared Allard <jaredallard@users.noreply.github.com>
2023-12-02 07:59:38 -0800

    use more oh-my-zsh, add back wsl.plugin.zsh

--- dot_local/share/dotfiles/zshrc.d/pre-zgenom-load/macOS.plugin.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-# shellcheck shell=bash
-
-# Ensure we have access to brew on macOS if it's installed
-if [[ "$OS" == "darwin" ]] && ! command -v "brew" >/dev/null && [[ -e /opt/homebrew/bin/brew ]]; then
-  eval "$(/opt/homebrew/bin/brew shellenv)"
-fi

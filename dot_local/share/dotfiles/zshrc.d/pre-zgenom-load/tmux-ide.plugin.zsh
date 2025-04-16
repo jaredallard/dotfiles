c60a43da6a63eb255db75ac25ae6311f8d42feb1
Jared Allard <jaredallard@users.noreply.github.com>
2025-04-16 15:54:58 -0700

    fix: use upstream theme, remove unused files
    Beforehand, I used a forked version of amrc which also happened to
    trigger some sort of race condition where `direnv` would be ran multiple
    times. Now, I insted configure my PS1 through the `amrc` which is now
    tracked.

--- dot_local/share/dotfiles/zshrc.d/pre-zgenom-load/tmux-ide.plugin.zsh
+++ dot_local/share/dotfiles/zshrc.d/pre-zgenom-load/tmux-ide.plugin.zsh
@@ -1,6 +1,6 @@
 export ZSH_TMUX_AUTOSTART=true
 
-# If we're running under cursor or VS Code, do not try to autostart tmux.
+# If we're running under an editor, do not try to autostart tmux.
 if pstree -p $$ | grep -Ei "cursor|vscode|visual studio code|zed" | grep -vq "grep"; then
   export ZSH_TMUX_AUTOSTART=false
   export ZSH_TMUX_AUTOCONNECT=false

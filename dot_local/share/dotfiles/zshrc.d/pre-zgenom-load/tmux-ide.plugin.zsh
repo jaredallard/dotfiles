bf6237875b60514aec948d373ce33c0905ec42e6
Jared Allard <jaredallard@users.noreply.github.com>
2025-04-22 10:34:04 -0700

    track gitconfig, sync misc changes/fixes

--- dot_local/share/dotfiles/zshrc.d/pre-zgenom-load/tmux-ide.plugin.zsh
+++ dot_local/share/dotfiles/zshrc.d/pre-zgenom-load/tmux-ide.plugin.zsh
@@ -1,7 +1,7 @@
 export ZSH_TMUX_AUTOSTART=true
 
 # If we're running under an editor, do not try to autostart tmux.
-if pstree -p $$ | grep -Ei "cursor|vscode|visual studio code|zed" | grep -vq "grep"; then
+if [[ "$TERM_PROGRAM" == "vscode" ]]; then
   export ZSH_TMUX_AUTOSTART=false
   export ZSH_TMUX_AUTOCONNECT=false
 fi
